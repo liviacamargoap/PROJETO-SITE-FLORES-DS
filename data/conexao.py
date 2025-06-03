410452a6b09b52f3f02adb101845c1545bbffd8e
Matheus Romagnoli <matheuszothavio@gmail.com>
2025-06-03 10:45:29 -0300

    ADD: criação da pasta migration e da pasta DATA, para mover o banco de dados para o migration e criar a conexão no DATA

---
+++ data/conexao.py
@@ -0,0 +1,11 @@
+import mysql.connector
+class Conexao: 
+    def criar_conexao():
+        conexao = mysql.connector.connect(
+            host = "localhost",
+            port = 3306,
+            user = "root",
+            password = "root",
+            database = "dbFeedback"
+    )
+        return conexao
