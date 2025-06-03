476cb5d74a22f6f267f1155855815d45df5e863b
Matheus Romagnoli <matheuszothavio@gmail.com>
2025-06-03 10:19:02 -0300

    MODIFY: ajuste

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, redirect, session
 
 app = Flask(__name__)
 
-app.secret_key = "banana"
+app.secret_key = "key"
 
 @app.route("/")
 def pagina_inicial():
@@ -12,7 +12,7 @@ def pagina_inicial():
 def logar():
     # session = uma lista | guardar informações do usuario
     # criou uma lista "usuario" para guardar as listas (organizar)
-    session["nome"] = "Godofredo"
+    session["nome"] = "Usuário"
     session["foto"] = "https://img.freepik.com/fotos-premium/foto-de-grande-angular-de-uma-unica-arvore-crescendo-sob-um-ceu-nublado-durante-um-por-do-sol-cercado-por-grama_181624-22807.jpg?semt=ais_hybrid&w=740"
     return redirect("/")
 
