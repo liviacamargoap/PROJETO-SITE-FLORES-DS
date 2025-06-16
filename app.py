df3237908cf9d9ce3b4a930b0f34f8825bddd6fb
Pietro <pietro.g.souza@aluno.senai.br>
2025-06-16 10:55:35 -0300

    MODIFY> login e logoff nomenclatura

--- app.py
+++ app.py
@@ -9,16 +9,16 @@ app.secret_key = "key"
 def pagina_inicial():
     return render_template("inicial.html")
 
-@app.route("/logar")
-def logar():
+@app.route("/login")
+def login():
     # session = uma lista | guardar informações do usuario
     # criou uma lista "usuario" para guardar as listas (organizar)
     session["nome"] = "Usuário"
     session["foto"] = "https://img.freepik.com/fotos-premium/foto-de-grande-angular-de-uma-unica-arvore-crescendo-sob-um-ceu-nublado-durante-um-por-do-sol-cercado-por-grama_181624-22807.jpg?semt=ais_hybrid&w=740"
     return redirect("/")
 
-@app.route("/deslogar")
-def deslogar():
+@app.route("/logoff")
+def logoff():
     session.clear()
     return redirect("/")
 
